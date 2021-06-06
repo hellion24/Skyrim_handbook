427f44232b5983c70b649462a8676168dc583023
Ivan <Ivan50592@yandex.ru>
2021-06-07 01:27:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kursovaya/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.kursovaya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static java.lang.Thread.sleep;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        Thread logoTimer = new Thread() {
+            public void run() {
+                try {
+                    int logoTimer = 0;
+                    while(logoTimer < 1000) {
+                        sleep(10000);
+                        logoTimer = logoTimer +100;
+                    };
+                    Intent i=new Intent(MainActivity.this,Menu.class);
+                    startActivity(i);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally{
+                    finish();
+                }
+            }
+        };
+        logoTimer.start();
+    }
+}
---
+++ app/src/main/java/com/example/kursovaya/Menu.java
@@ -0,0 +1,26 @@
+package com.example.kursovaya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Menu extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+
+
+
+    }
+}
